0dcc261dccfd242c05c4e0299abda299a16fc342
Alexandra <AAV998@yandex.ru>
2021-10-02 12:38:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // 1.43*x = 19.8*y''' + 114.6*y'' + 43.9*y' + y
+        }
+    }
+}
